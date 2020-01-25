d31d005681b3bcec31ccdab9bdb4ef9ea5e86128
Jaen Marco <jeanmistic08@gmail.com>
2020-01-24 19:38:29 -0500

    Subiendo proyecto

---
+++ Practica3/src/test/java/com/jaen/Practica3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
